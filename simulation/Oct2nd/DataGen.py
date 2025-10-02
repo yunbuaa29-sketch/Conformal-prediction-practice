8f582522fcbee57cedb80b5752a42fb2c8e44047
yunbuaa29-sketch <yunbuaa29@gmail.com>
2025-10-02 14:01:13 -0400

    Update DataGen.py

--- simulation/Oct2nd/DataGen.py
+++ simulation/Oct2nd/DataGen.py
@@ -1,5 +1,4 @@
 ### Generate Y following descrete distriution with label 0,1,2 each with constant probability
-### p1 = 0.2, p2 = 0.5, p3 = 0.3
 ### Generate X following normal ditribution
 ### funture improvement: define pmf
 import numpy as np
@@ -54,12 +53,8 @@ def Simple_generater(number = 1000, label = [0, 1, 2], probss = [0.1,0.3,0.6], p
     else:
         x_test = np.random.normal(loc=m3, scale=sigma3, size = 1)
 
-    # Combine
-    # sample = pd.DataFrame({"X": x_sample,"Y": y_sample})
-    # test = pd.DataFrame({"X": x_test,"Y": y_test})
-    # print(sample[0:10])
-
     return x_sample, y_sample, x_test, y_test
 
-Simple_generater()
+
+
 
